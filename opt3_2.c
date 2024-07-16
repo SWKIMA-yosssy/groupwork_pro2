d00ab0f77aa5c8092338059b63bf0723db0e0f1f
SWKIMA-arch <greyonth@gmail.com>
2024-07-16 17:15:12 +0900

    unified 2_opt and 3_opt but there is no way to create outcome opt3_2

--- opt3_2.c
+++ opt3_2.c
@@ -1,20 +1,55 @@
-void opt3_2(int *new_path, int n, double *new_path_length) {
-  double path_length[7]; // store total length of each roots which are created
-                         // by n-opt
-  // i=0~2  => for 2-opt, other is for 3-opt
-  int selected_nodes[3]; // store selected node numbers used in n-opt
-  int opted_path[7][n];  // store total node list of each roots
+void opt_3_2_cal_length(int *pre_path, int n, int *selected_nodes,
+                        double *path_length, int whether_geograph) {
+  int A = selected_nodes[0], B = pre_path[(selected_nodes[0] + 1) % n];
+  int C = selected_nodes[1], D = pre_path[(selected_nodes[1] + 1) % n];
+  int E = selected_nodes[2], F = pre_path[(selected_nodes[2] + 1) % n];
+  // initial state of node are circle: state 3 in paper
+  if (whether_geograph == 0) { // data is given as euclidean data
+    path_length[0] =
+        euclid_distance(A, E) + euclid_distance(B, D) + euclid_distance(C, F);
+    path_length[1] =
+        euclid_distance(A, D) + euclid_distance(E, C) + euclid_distance(B, F);
+    path_length[2] =
+        euclid_distance(A, C) + euclid_distance(B, E) + euclid_distance(D, F);
+    path_length[3] =
+        euclid_distance(A, C) + euclid_distance(B, D) + euclid_distance(E, F);
+    path_length[4] =
+        euclid_distance(A, B) + euclid_distance(C, E) + euclid_distance(D, F);
+    path_length[5] =
+        euclid_distance(A, D) + euclid_distance(E, B) + euclid_distance(C, F);
+    path_length[6] =
+        euclid_distance(A, E) + euclid_distance(D, C) + euclid_distance(B, F);
+  } else {
+    path_length[0] = geograph_distance(A, E) + geograph_distance(B, D) +
+                     geograph_distance(C, F);
+    path_length[1] = geograph_distance(A, D) + geograph_distance(E, C) +
+                     geograph_distance(B, F);
+    path_length[2] = geograph_distance(A, C) + geograph_distance(B, E) +
+                     geograph_distance(D, F);
+    path_length[3] = geograph_distance(A, C) + geograph_distance(B, D) +
+                     geograph_distance(E, F);
+    path_length[4] = geograph_distance(A, B) + geograph_distance(C, E) +
+                     geograph_distance(D, F);
+    path_length[5] = geograph_distance(A, D) + geograph_distance(E, B) +
+                     geograph_distance(C, F);
+    path_length[6] = geograph_distance(A, E) + geograph_distance(D, C) +
+                     geograph_distance(B, F);
+  }
+}
+void opt3_2_once(int *new_path, int n, double *new_path_length, int node1,
+                 int node2, int node3, int whether_geograph) {
+  double path_length[7];
+  int selected_nodes[3] = {node1, node2,
+                           node3}; // store selected node numbers used in n-opt
+                                   // int opted_path[7][n];  // store total node
+                                   // list of each roots. ith global array.
   double min_path_length = *new_path_length;
   int min_path_num = -1; // number of index which gives minimum length
   // -1 means no change
   int i;
 
-  for (i = 0; i < 3; i++) {
-    selected_nodes[i] = createrand(n);
-  }
-
-  opt_2(new_path, selected_nodes, opted_path, path_length);
-  opt_3(new_path, selected_nodes, opted_path, path_length);
+  opt_3_2_cal_length(new_path, n, selected_nodes, path_length,
+                     whether_geograph);
 
   for (i = 0; i < 7; i++) {
     if (min_path_length > path_length[i]) {
@@ -26,8 +61,6 @@ void opt3_2(int *new_path, int n, double *new_path_length) {
   // reflect outcome
   if (min_path_num != -1) {
     *new_path_length = min_path_length;
-    for (i = 0; i < n; i++) {
-      new_path[i] = opted_path[min_path_num][i];
-    }
+  } else if (i == 0) {
   }
 }

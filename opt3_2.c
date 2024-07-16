079bd1fa29a62585d1ceee5d05419dd1ec8d9087
SWKIMA-arch <greyonth@gmail.com>
2024-07-16 14:32:49 +0900

    first commit of opt3_2

---
+++ opt3_2.c
@@ -0,0 +1,33 @@
+void opt3_2(int *new_path, int n, double *new_path_length) {
+  double path_length[7]; // store total length of each roots which are created
+                         // by n-opt
+  // i=0~2  => for 2-opt, other is for 3-opt
+  int selected_nodes[3]; // store selected node numbers used in n-opt
+  int opted_path[7][n];  // store total node list of each roots
+  double min_path_length = *new_path_length;
+  int min_path_num = -1; // number of index which gives minimum length
+  // -1 means no change
+  int i;
+
+  for (i = 0; i < 3; i++) {
+    selected_nodes[i] = createrand(n);
+  }
+
+  opt_2(selected_nodes, opted_path, path_length);
+  opt_3(selected_nodes, opted_path, path_length);
+
+  for (i = 0; i < 7; i++) {
+    if (min_path_length > path_length[i]) {
+      min_path_num = i;
+      min_path_length = path_length[i];
+    }
+  }
+
+  // reflect outcome
+  if (min_path_num != -1) {
+    *new_path_length = min_path_length;
+    for (i = 0; i < n; i++) {
+      new_path[i] = opted_path[min_path_num][i];
+    }
+  }
+}

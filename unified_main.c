7ecfaee996cdc055d7a3cb59107fff025c3b4e55
SWKIMA <146754361+SWKIMA-yosssy@users.noreply.github.com>
2024-07-18 12:09:29 +0900

    Update unified_main.c

--- unified_main.c
+++ unified_main.c
@@ -25,14 +25,14 @@ double euclid_distance(struct city *cities, int a, int b) {
 double deg2rad(double deg) { return deg * (M_PI / 180.0); }
 
 /*地理的距離を求める関数*/
-double haversine(double lat1, double lon1, double lat2, double lon2) {
-  double dlat = deg2rad(lat2 - lat1);
-  double dlon = deg2rad(lon2 - lon1);
-  double a = sin(dlat / 2) * sin(dlat / 2) + cos(deg2rad(lat1)) *
-                                                 cos(deg2rad(lat2)) *
-                                                 sin(dlon / 2) * sin(dlon / 2);
-  double c = 2 * atan2(sqrt(a), sqrt(1 - a));
-  return earthr * c;
+double giograph_distance(struct city *cities, int a, int b) {
+    double dlat = deg2rad(cities[a].x-cities[b].x);
+    double dlon = deg2rad(cities[a].y-cities[b].y);
+    double A = sin(dlat / 2) * sin(dlat / 2) +
+               cos(deg2rad(cities[b].x)) * cos(deg2rad(cities[a].x)) *
+               sin(dlon / 2) * sin(dlon / 2);
+    double C = 2 * atan2(sqrt(A), sqrt(1 - A));
+    return earthr * C;
 }
 double distance(
     int *path, int n,

274e9e31faad99f33d607ab9edc26baeb72d76ce
SWKIMA <146754361+SWKIMA-yosssy@users.noreply.github.com>
2024-07-19 23:02:46 +0900

    Update unified_main.c

--- unified_main.c
+++ unified_main.c
@@ -72,42 +72,45 @@ double distance(
 // visited[farthest]=1;}
 
 // 最も遠い点を見つける関数
-int find_farthest_node(int N, int visited[maxN]) {
-  int farthest = -1;
-  double max_dist = -1;
-  for (int i = 0; i < N; i++) {
-    if (visited[i] == -1) {
-      for (int j = 0; j < N; j++) {
-        if (visited[j] == -1 && dist[i][j] > max_dist) {
-          max_dist = dist[i][j];
-          farthest = i;
+int find_farthest_node(double dist[][MAXN], int N, int visited[MAXN]) {
+    int farthest = -1;
+    double max_dist = -1;
+    for (int i = 0; i < N; i++) {
+        if (visited[i] == -1) {
+            double sum_dist = 0;
+            for (int j = 0; j < N; j++) {
+                if (visited[j] != -1) {
+                    sum_dist += dist[i][j];
+                }
+            }
+            if (sum_dist > max_dist) {
+                max_dist = sum_dist;
+                farthest = i;
+            }
         }
-      }
     }
-  }
-  return farthest;
+    return farthest;
 }
 
 // 点をツアーに挿入する関数
-void insert_nodes(int new_path[maxN], int *path_size, int node) {
-  int best_position = -1;
-  double best_cost;
+void insert_nodes(int new_path[MAXN], int *path_size, double dist[][MAXN], int node) {
+    int best_position = 0;
+    double best_cost = 10000.0;
 
-  for (int i = 0; i < *path_size; i++) {
-    int j = (i + 1) % *path_size;
-    double cost = dist[new_path[i]][node] + dist[node][new_path[j]] -
-                  dist[new_path[i]][new_path[j]];
-    if (cost < best_cost) {
-      best_cost = cost;
-      best_position = j;
+    for (int i = 0; i < *path_size; i++) {
+        int j = (i + 1) % *path_size;
+        double cost = dist[new_path[i]][node] + dist[node][new_path[j]] - dist[new_path[i]][new_path[j]];
+        if (cost < best_cost) {
+            best_cost = cost;
+            best_position = j;
+        }
     }
-  }
 
-  for (int i = *path_size; i > best_position; i--) {
-    new_path[i] = new_path[i - 1];
-  }
-  new_path[best_position] = node;
-  (*path_size)++;
+    for (int i = *path_size; i > best_position; i--) {
+        new_path[i] = new_path[i - 1];
+    }
+    new_path[best_position] = node;
+    (*path_size)++;
 }
 void opt_3_2_cal_length(struct city *cities, int *pre_path, int n,
                         int *selected_nodes, double *path_length,
